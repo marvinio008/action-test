f9a5921285b6064e21d7d105c18389535741ebe9
marvinio008 <41358708+marvinio008@users.noreply.github.com>
2018-07-25 14:03:46 +0200

    Update index.js

--- index.js
+++ index.js
@@ -20,15 +20,15 @@ restService.post("/echo", function(req, res) {
        
      case "test" :
        
-       speech = "test1;
+       speech = "test1";
        break;
        
      case "testen" :
        speech = "testen";
        break;
        
-     case "testenn" : 
-       speech = "testenn";
+     case "test4" : 
+       speech = "test4";
        break;
    }
      
