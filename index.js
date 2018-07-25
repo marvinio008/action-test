67ff9054f840074df14da0dcc8437344cc40c009
marvinio008 <41358708+marvinio008@users.noreply.github.com>
2018-07-25 13:17:26 +0200

    Update index.js

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ restService.use(
 
 restService.use(bodyParser.json());
 
-restService.post(/standorte, function(req,res) {
+restService.post("/echo", function(req,res) {
    var speech =
        req.body.result &&
     req.body.result.parameters &&
