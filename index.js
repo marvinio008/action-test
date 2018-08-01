f3d2d2b3295f58a9c7fbd221d4da0326aae0b564
marvinio008 <41358708+marvinio008@users.noreply.github.com>
2018-08-01 08:15:44 +0200

    Update index.js

--- index.js
+++ index.js
@@ -24,6 +24,22 @@ restService.post("/wertstoffhoefe", function(req, res) {
       
   return res.json({
     fulfillmentText: speech,
+    fulfillmentMessages: [
+    {
+      "card": {
+        "title": "card title",
+        "subtitle": "card text",
+        "imageUri": "https://assistant.google.com/static/images/molecule/Molecule-Formation-stop.png",
+        "buttons": [
+          {
+            "text": "button text",
+            "postback": "https://assistant.google.com/"
+          }
+        ]
+      }
+    }
+  ],
+
     source: "DialogflowTest"
   });
 });
